cfb865d26057906b40bd7b5e4d2f1d5eb2654282
Marcus Greiner <myeyetea@posteo.de>
2020-08-27 20:12:10 +0200

    Implemented logic to clear playing field when all fields are set Implemented logic for changing content of a field Implemented class Player

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TicTacToe
 {
@@ -20,9 +8,86 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly Player player = new Player();
+        readonly Player player1 = player;
+
+        public class Player
+        {
+            public string PlayerCharacter { get; set; }
+
+            public Player() => PlayerCharacter = "X";
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            ClearGrid();
+
+        }
+
+        private void ToDoWhenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button senderAsButton = (Button)sender;
+            var actualBackgroundColor = senderAsButton.Background;
+            senderAsButton.Background = senderAsButton.Foreground;
+            senderAsButton.Foreground = actualBackgroundColor;
+
+            try
+            {
+                if (StartNewGame())
+                {
+                    ClearGrid();
+                    player1.PlayerCharacter = "X";
+                }
+
+                if (senderAsButton.Content == null || senderAsButton.Content.ToString() == "")
+                {
+                    if (player1.PlayerCharacter == "O")
+                    {
+                        senderAsButton.Content = "O";
+                        player1.PlayerCharacter = "X";
+                    }
+                    else if(player1.PlayerCharacter == "X")
+                    {
+                        senderAsButton.Content = "X";
+                        player1.PlayerCharacter = "O";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Feld bereits belegt.");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private void ClearGrid()
+        {
+            kaestchen_0_0.Content = "";
+            kaestchen_1_0.Content = "";
+            kaestchen_2_0.Content = "";
+            kaestchen_0_1.Content = "";
+            kaestchen_1_1.Content = "";
+            kaestchen_2_1.Content = "";
+            kaestchen_0_2.Content = "";
+            kaestchen_1_2.Content = "";
+            kaestchen_2_2.Content = "";
+        }
+
+        private bool StartNewGame()
+        {
+            foreach (var item in Spielfeld.Children)
+            {
+                if (item is Button kaestchen && kaestchen.Content.ToString() == string.Empty)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
